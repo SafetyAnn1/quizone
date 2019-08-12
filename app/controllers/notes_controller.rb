a8b656b73b88d74bb4ff5d15ba74dca9f681cff7
Ann Meier <annmeier3@gmail.com>
2019-08-12 21:23:28 +0000

    Paginate and create Submission Form

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,11 +1,22 @@
 class NotesController < ApplicationController
   
   def index
-   @notes = Note.order("title").page(params[:page]).per_page(5)
+   @notes = Note.order("title").page(params[:page]).per_page(4)
   end
 
   def new
     @note = Note.new
+  end
+
+  def create
+    Note.create(note_params)
+    redirect_to root_path
+  end
+
+  private
+
+  def note_params
+    params.require(:note).permit(:title, :description)
   end
 
 end
